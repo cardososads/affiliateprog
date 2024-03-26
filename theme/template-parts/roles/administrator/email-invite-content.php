0b175637df5f6c43a28912d1d09b0b120910f4d4
cardososads <cardososads@github.com>
2024-03-26 01:07:12 -0400

    Melhorias no processo de Adição de usuarios

---
+++ theme/template-parts/roles/administrator/email-invite-content.php
@@ -0,0 +1,25 @@
+<h2 class="text-2xl font-semibold mb-5">Link de Afiliado</h2>
+<div class="flex flex-col gap-4">
+	<!--				<form action="" class="flex gap-4 w-[80%]">-->
+	<!--					<input class="rounded-lg border border-gray-300 px-4 py-2 flex-grow" type="text" placeholder="Insira seu link de afiliado aqui">-->
+	<!--					<button class="px-4 py-2 bg-green-600 text-white rounded-lg" type="submit">Compartilhar link</button>-->
+	<!--				</form>-->
+	<div class="p-6 bg-gray-100 flex flex-col gap-2 w-[90%] shadow rounded-lg">
+		<h3 class="text-2xl font-semibold mb-4">Convite via e-mail</h3>
+		<form action="" class="flex flex-col gap-4">
+			<div class="flex flex-col">
+				<label for="email">E-mail</label>
+				<input class="rounded-lg border border-gray-300 px-4 py-2" type="email" name="email" placeholder="E-mail">
+			</div>
+			<!--						<div class="flex flex-col">-->
+			<!--							<label for="empresa">Empresa</label>-->
+			<!--							<input class="rounded-lg border border-gray-300 px-4 py-2" type="text" name="empresa" placeholder="Nome da empresa">-->
+			<!--						</div>-->
+			<div class="flex flex-col">
+				<label for="cnpj">CNPJ</label>
+				<input class="rounded-lg border border-gray-300 px-4 py-2" type="text" name="cnpj" placeholder="CNPJ">
+			</div>
+			<button class="px-[30px] py-[10px] bg-green-600 text-white rounded-lg self-end" type="submit">Convidar</button>
+		</form>
+	</div>
+</div>
