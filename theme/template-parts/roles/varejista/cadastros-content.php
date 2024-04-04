2acb483604d0d4ad043939eb9d588d91a32d3537
cardososads <cardososads@github.com>
2024-04-03 22:19:30 -0400

    Convite por email - Ajustar o css

--- theme/template-parts/roles/varejista/cadastros-content.php
+++ theme/template-parts/roles/varejista/cadastros-content.php
@@ -40,7 +40,7 @@
 						</div>
 
 						<button type="submit"
-								class="bg-verde hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+								class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
 							Convidar
 						</button>
 					</form>
